61250b1c9cf67fb401f4dd85e6996452841ecfab
redaAssemghor <assemghor.reda@gmail.com>
2024-10-19 12:09:33 +0100

    mobile menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 import Navbar from "./Navbar";
 import ShimmerButton from "@/components/magicui/shimmer-button";
 import { useEffect, useRef, useState } from "react";
-import { CiMenuBurger } from "react-icons/ci";
 import { FaChevronDown } from "react-icons/fa6";
-import { RxCross1 } from "react-icons/rx";
 import { useGSAP } from "@gsap/react";
 import { burgerMenuAnimation } from "@/animations/fadeIn";
 import Image from "next/image";
+import Checkbox from "./ui/Menu";
+import Button from "./ui/Button";
 
 const Header = () => {
   const burgerRef = useRef(null);
@@ -16,6 +16,20 @@ const Header = () => {
   const [scrolled, setScrolled] = useState(false);
   const [openMenu, setOpenMenu] = useState(false);
   const [openLinks, setOpenLinks] = useState("");
+
+  const servicesLinks = [
+    { title: "Custom Web Design", link: "/web-design" },
+    { title: "Wordpress", link: "/wordpress" },
+    { title: "Shopify Website Design", link: "/shopify" },
+    { title: "Ecommerce Development", link: "/ecommerce-development" },
+    { title: "Chatbot Integration Services", link: "/chatbot" },
+  ];
+
+  const aboutLinks = [
+    { title: "Locations", link: "/contact" },
+    { title: "About Us", link: "/about-us" },
+    { title: "Contact Us", link: "/contact" },
+  ];
 
   const handleLinks = (link: string) => {
     setOpenLinks(openLinks === link ? "" : link);
@@ -45,21 +59,16 @@ const Header = () => {
 
   return (
     <nav
-      className={`flex bg-[--primary] justify-between items-center p-5 w-full z-50 fixed border-b-[--light] text-white duration-700 ${
+      className={`flex bg-[--primary] justify-between items-center p-2 w-full z-50 fixed border-b-[--light] text-white duration-700 ${
         scrolled ? "opacity-90" : "border-b-[0.1px]"
       }`}
     >
-      <Link href={"/"}>
+      <Link className="z-50" href={"/"}>
         <Image src="/logo-name.png" alt="logo" height={64} width={160} />
       </Link>
-      {/* Mobile Menu Toggle Button */}
-      <button
-        ref={burgerRef}
-        className="block z-50 lg:hidden"
-        onClick={handleMenu}
-      >
-        {openMenu ? <RxCross1 size={40} /> : <CiMenuBurger size={40} />}
-      </button>
+      <div className="z-50">
+        <Checkbox handleMenu={handleMenu} />
+      </div>
 
       {/* Desktop Menu */}
       <div className="hidden lg:flex gap-10 items-center">
@@ -75,51 +84,33 @@ const Header = () => {
 
       {/* Mobile Menu */}
       <div
-        className={`fixed px-10 py-36 top-0 right-0 z-40 w-full h-screen bg-[--primary] flex flex-col items-center justify-center transition-transform duration-700 ${
+        className={`fixed top-0 right-0 p-3 z-40 w-full h-screen bg-[--primary] flex flex-col transition-transform duration-700 ${
           openMenu ? "translate-x-0" : "translate-x-full"
         } lg:hidden`}
       >
         {/* Services Section */}
-        <div className="flex flex-col gap-10 w-3/4">
+        <div className="flex flex-col w-full pt-28 space-y-6">
           <div>
             <button
               onClick={() => handleLinks("services")}
-              className="flex gap-2 pb-4 items-center font-bold text-2xl justify-between w-full"
+              className="flex items-center font-bold text-lg justify-between w-full"
             >
               Services <FaChevronDown />
             </button>
             <ul
-              className={`flex flex-col gap-4 text-sm text-[--text1] mt-2 transition-all duration-700 ${
+              className={`flex flex-col gap-4 text-sm text-[--text1] mt-2 ${
                 openLinks === "services"
                   ? "max-h-40"
                   : "max-h-0 overflow-hidden"
               }`}
             >
-              <li className="hover:underline transition duration-300">
-                <Link onClick={handleMenu} href={"/web-design"}>
-                  Custom Web Design
-                </Link>
-              </li>
-              <li className="hover:underline transition duration-300">
-                <Link onClick={handleMenu} href={"/wordpress"}>
-                  Wordpress
-                </Link>
-              </li>
-              <li className="hover:underline transition duration-300">
-                <Link onClick={handleMenu} href={"/shopify"}>
-                  Shopify Website Design
-                </Link>
-              </li>
-              <li className="hover:underline transition duration-300">
-                <Link onClick={handleMenu} href={"/ecommerce-development"}>
-                  Ecommerce Development
-                </Link>
-              </li>
-              <li className="hover:underline transition duration-300">
-                <Link onClick={handleMenu} href={"/chatbot"}>
-                  Chatbot Integration Services
-                </Link>
-              </li>
+              {servicesLinks.map((service, i) => (
+                <li key={i} className="hover:underline transition duration-300">
+                  <Link onClick={handleMenu} href={service.link}>
+                    {service.title}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
 
@@ -127,30 +118,22 @@ const Header = () => {
           <div>
             <button
               onClick={() => handleLinks("about")}
-              className="flex gap-2 pb-4 items-center font-bold text-2xl justify-between w-full"
+              className="flex gap-2 pb-4 items-center font-bold text-lg justify-between w-full"
             >
               About <FaChevronDown />
             </button>
             <ul
-              className={`flex flex-col gap-4 text-sm text-[--text1] mt-2 transition-all duration-700 ${
+              className={`flex flex-col gap-4 text-sm text-[--text1] mt-2 ${
                 openLinks === "about" ? "max-h-40" : "max-h-0 overflow-hidden"
               }`}
             >
-              <li className="hover:underline transition duration-300">
-                <Link onClick={handleMenu} href={"/contact"}>
-                  Locations
-                </Link>
-              </li>
-              <li className="hover:underline transition duration-300">
-                <Link onClick={handleMenu} href={"/about-us"}>
-                  About Us
-                </Link>
-              </li>
-              <li className="hover:underline transition duration-300">
-                <Link onClick={handleMenu} href={"/contact"}>
-                  Contact Us
-                </Link>
-              </li>
+              {aboutLinks.map((about, i) => (
+                <li key={i} className="hover:underline transition duration-300">
+                  <Link onClick={handleMenu} href={about.link}>
+                    {about.title}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
 
@@ -158,16 +141,14 @@ const Header = () => {
           <Link
             onClick={handleMenu}
             href={"/#projects"}
-            className="flex gap-2 items-center font-bold text-2xl justify-between w-full"
+            className="flex gap-2 items-center font-bold text-lg justify-between w-full"
           >
             Projects
           </Link>
         </div>
-        <div className="mt-24">
+        <div className="pb-5 mt-auto">
           <Link href="/contact">
-            <ShimmerButton className="mt-auto">
-              REQUEST YOUR QUOTE
-            </ShimmerButton>
+            <Button handleClick={handleMenu} />
           </Link>
         </div>
       </div>
